59168494ee139e8543d75801de4f5ef6836dd5d2
rafalsiwiec <rafal.siwiec@getmoss.com>
2024-12-05 08:37:12 +0100

    Integration tests separated from API tests

--- src/test/kotlin/pl/fnfcinema/cinema/ApiTest.kt
+++ src/test/kotlin/pl/fnfcinema/cinema/ApiTest.kt
@@ -3,16 +3,14 @@ package pl.fnfcinema.cinema
 import org.junit.jupiter.api.BeforeEach
 import org.mockito.Mockito
 import org.mockito.Mockito.mock
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
-import org.springframework.boot.test.context.SpringBootTest
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest
 import org.springframework.boot.test.context.TestConfiguration
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Import
 import org.springframework.context.annotation.Primary
 import pl.fnfcinema.cinema.movies.Movies
 
-@SpringBootTest
-@AutoConfigureMockMvc
+@WebMvcTest
 @Import(ApiTest.Config::class)
 abstract class ApiTest {
 
--- src/test/kotlin/pl/fnfcinema/cinema/IntegrationTest.kt
+++ src/test/kotlin/pl/fnfcinema/cinema/IntegrationTest.kt
@@ -3,7 +3,6 @@ package pl.fnfcinema.cinema
 import org.junit.jupiter.api.BeforeEach
 import org.mockito.Mockito
 import org.mockito.Mockito.mock
-import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc
 import org.springframework.boot.test.context.SpringBootTest
 import org.springframework.boot.test.context.TestConfiguration
 import org.springframework.context.annotation.Bean
@@ -12,7 +11,6 @@ import org.springframework.context.annotation.Primary
 import pl.fnfcinema.cinema.integrations.imdb.ImdbApi
 
 @SpringBootTest
-@AutoConfigureMockMvc
 @Import(IntegrationTest.Config::class)
 abstract class IntegrationTest {
 
